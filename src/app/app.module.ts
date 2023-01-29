7b735c8c57c3f8bb88b97930bc737932d942fb73
cjoshi <cjoshi@live.in>
2023-01-29 12:06:08 +0530

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,48 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ButtonModule} from 'primeng/button';
+//*****************Core**************************/
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from 'primeng/ripple';
 import { FormsModule } from '@angular/forms';
-import {RippleModule} from 'primeng/ripple';
+//*****************Core**************************/
+
+//*****************Firebase**************************/
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+//*****************Firebase**************************/
+
+//*****************Component**************************/
+
+import { ButtonModule } from 'primeng/button';
+
+//*****************Component**************************/
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    //core
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     RippleModule,
-    ButtonModule
+    FormsModule,
+
+    //Firebase
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+
+    //Component
+    ButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDxhiAr9Zc_6wydzJzeeQeKlvDPCquxge4',
+    authDomain: 'cjoshi-logistics.firebaseapp.com',
+    projectId: 'cjoshi-logistics',
+    storageBucket: 'cjoshi-logistics.appspot.com',
+    messagingSenderId: '594260998551',
+    appId: '1:594260998551:web:40a48d12965b82684becd2',
+  },
+};
