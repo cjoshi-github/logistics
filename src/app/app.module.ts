81229db7aa45a3ac328fe12313fa1044cbfc1430
cjoshi <cjoshi@live.in>
2023-01-29 11:42:37 +0530

    project setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import {RippleModule} from 'primeng/ripple';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    RippleModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
